838b5e5c5a2d9ad9519249132f15abf55f45eced
Andrew <arhq315@gmail.com>
2016-09-16 17:28:52 -0500

    Added Inital Data
    Added initial JSON data for: banners, posters, real estate signs, yard
    signs, and corrugated plastic.

---
+++ js/price-calc-config.js
@@ -0,0 +1,87 @@
+priceCalc.config= {
+  banners: {
+    "13oz Vinyl #1401": {
+      area: 4.50,
+      unit: "ft"
+    },
+    "8oz Mesh #1403": {
+      area: 5,
+      unit: "ft"
+    },
+    "13oz Vinyl 18oz Double Sided #1402": {
+      area: 9.98,
+      unit: "ft"
+    }
+  },
+  "corrugated plastic": {
+    "one sided": {
+      qty: ["10:13.68", "25:9.05", "50:8.72"]
+    },
+    "two sided": {
+      qty: ["10:17.30", "25:11.68", "50:11.01"]
+    },
+  },
+  "real estate signage with frame (2 sided)": {
+    "24x18": {
+      qty: ["1:77.91", "5:53.58", "10:47.91", "20:46.24"],
+      extra: "variations:6",
+      updgrade: "1/2\" Radius Corners:1"
+    },
+    "28x20": {
+      qty: ["1:94.58", "5:72.58", "10:64.58", "20:62.91"],
+      extra: "variations:6",
+      updgrade: "1/2\" Radius Corners:1"
+    },
+    "36x24": {
+      qty: ["1:105.41", "5:82.08", "10:75.41", "20:73.74"],
+      extra: "variations:6",
+      updgrade: "1/2\" Radius Corners:1"
+    }
+  },
+  "sidewalk signage (23x35)": {
+    qty: ["2:60","4:58","12:54","16:53"],
+    extra: "variations:6"
+  },
+  "vinyl posters": {
+     "Intermediate Vinyl #2009": {
+       area: 5,
+       upgrade: "Gloss Overlaminate:3"
+     },
+     "Air Egress Vinyl #2009X": {
+       area: 6,
+       upgrade: "Gloss Overlaminate:3"
+     },
+     "Translucent Backlit Vinyl #2007": {
+       area: 12,
+       upgrade: "Gloss Overlaminate:3"
+     },
+     "Perforated Vinyl #2005": {
+       area: 10
+     },
+     "Premium Glossy Paper #2400": {
+       area: 5
+     },
+     "Intermediate Vinyl #1530": {
+       area: 8,
+       "perimiter-upgrade!!": "Magnetic Adhesive:6"
+     }
+  },
+  "yard signs": {
+    "24x12": {
+      "one sided": {
+        qty: ['1:25','10:5.45','25:4.35','50:4.02','100:3.85','250:3.75','500:3.72','1000:3.70']
+      },
+      "two sided": {
+        qty: ['1:25','10:6.92','25:5.57','50:5.24','100:5.07','250:4.97','500:4.94','1000:4.92']
+      }
+    },
+    "24x18": {
+      "one sided": {
+        qty: ['1:28.53','10:9.51','25:6.55','50:6.22','100:6.05','250:5.95','500:5.83','1000:5.67']
+      },
+      "two sided": {
+        qty: ['1:34.41','10:11.47','25:8.51','50:8.18','100:8.01','250:7.91','500:7.83','1000:7.67']
+      }
+    }
+  }
+};
