1a14f3219368b1246d189c3d9c809c4dd257cb1c
Andrew <arhq315@gmail.com>
2016-09-21 13:43:33 -0500

    Added Form Generation to Output
    Added code to generate dropdowns lists and item.

--- js/price-calc.js
+++ js/price-calc.js
@@ -1,6 +1,54 @@
 'use strict';
+//module basics
 window.priceCalc={};
 priceCalc.config={};
 priceCalc.input={};
 priceCalc.logic={};
 priceCalc.output={};
+
+//input
+priceCalc.input.item = function (target) {
+  var form = target.parentElement;
+  var obj = priceCalc.config;
+  form.querySelectorAll('select').forEach(function (dropdown) {
+    obj = obj[dropdown.value];
+  });
+  var func = priceCalc.logic.isItem(obj) ? 'item' : 'list';
+  priceCalc.output[func](obj,form);
+};
+
+//logic
+priceCalc.logic.isItem = function (obj) {
+  return obj.area || obj.qty;
+};
+
+//output
+priceCalc.output.area = function (obj) {
+};
+priceCalc.output.init = function () {
+  priceCalc.output.list();
+};
+priceCalc.output.item = function (obj, form) {
+  var form = form || document.querySelector('form');
+  var func = obj.area ? 'area' : 'qty';
+  priceCalc.output[func](obj, form);
+};
+priceCalc.output.list = function (obj, form) {
+  var form = form || document.querySelector('form');
+  var obj = obj || priceCalc.config;
+  var list = document.createElement('select');
+  list.onchange = function (event) {
+    priceCalc.input.item(event.target);
+  };
+  var arr= Object.keys(obj);
+  arr.unshift('');
+  arr.forEach(function (key) {
+    var item = document.createElement('option');
+    item.innerHTML = key;
+    item.value = key;
+    list.appendChild(item);
+  });
+  form.appendChild(list);
+};
+priceCalc.output.qty = function (obj) {
+};
