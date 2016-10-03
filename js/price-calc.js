d9a0df3d32c8cca3aaa50b12d9c2cd5a891d7092
Andrew <arhq315@gmail.com>
2016-10-03 17:47:44 -0500

    Added Live Price Calculation
    Implemented live price calculations that can handle area and quantity
    based pricing.

--- js/price-calc.js
+++ js/price-calc.js
@@ -8,23 +8,55 @@ priceCalc.output={};
 
 //input
 priceCalc.input.calc = function (event) {
+  priceCalc.output.price();
 };
 priceCalc.input.item = function (target) {
   var form = document.querySelector('.price-calc .list');
   var obj = priceCalc.config;
+  //serialize here!!
   //remove all dropdowns after this target!!
   form.querySelectorAll('select').forEach(function (dropdown) {
     obj = obj[dropdown.value];
   });
+  //clear fieldset.item here!!
   var func = priceCalc.logic.isItem(obj) ? 'item' : 'list';
-  console.log(func);
   priceCalc.output[func](obj);
+  //re-output if possible here!!
 };
 priceCalc.input.serialize = function () {
   var obj = {};
   return obj;
 };
 //logic
+priceCalc.logic.capitalize = function(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+priceCalc.logic.getBasePrice = function (obj, area) {
+  var base = obj.price;//will be undefined if there is another price-based property
+  if (obj.qty) {
+    base = priceCalc.logic.getBracketPrice(obj.qty, area);
+    //determine which bracket the order falls in
+  } else if (obj.area) {
+    base = obj.area;
+  }
+  //return something else if over max?!!
+  return base;
+};
+priceCalc.logic.getBracketPrice = function (qty, area) {
+  var arr = qty.reverse();
+  var price = 0;
+  for (var c = 0, l = arr.length; c < l; c++) {
+    var entry = arr[c].split(':').map(function(str) {
+      return parseFloat(str);
+    });
+    var minQty = entry[0];
+    price = entry[1];
+    if (area >= minQty) {
+      break;
+    }
+  }
+  return price;
+};
 priceCalc.logic.isItem = function (obj) {
   return obj.area || obj.qty || obj.price;
 };
@@ -39,11 +71,9 @@ priceCalc.output.area = function (obj) {
 };
 priceCalc.output.dimension = function (dimension, unit) {
   var label = $('<label>');
-  var desc = $('<span>').html(dimension);
+  var desc = $('<span>').html(priceCalc.logic.capitalize(dimension) + ': ');
   //opted for onclick to avoid memory leaks down the road
-  var input = $('<input>').attr('type', 'number').addClass(dimension).attr('onclick', function (event) {
-    priceCalc.input.calc(event);
-  });
+  var input = $('<input>').attr('type', 'number').attr('min', '0').val(1).attr('oninput', 'priceCalc.input.calc()').addClass(dimension);
   var unit = $('<span>').html(unit);
   return label.append(desc).append(input).append(unit);
 };
@@ -57,7 +87,8 @@ priceCalc.output.item = function (obj) {
   if (obj.area) {
     item.append(priceCalc.output.area(obj));
   }
-  //generate qty here!!
+  item.append(priceCalc.output.qty(obj));
+  priceCalc.output.price();
 };
 priceCalc.output.list = function (obj) {
   var obj = obj || priceCalc.config;
@@ -74,4 +105,30 @@ priceCalc.output.list = function (obj) {
     list.appendChild(item);
   });
   document.querySelector('.price-calc .list').appendChild(list);
+};
+priceCalc.output.obj = function () {
+  var obj = priceCalc.config;
+  $('.price-calc .list select').each(function () {
+    var prop = this.value;
+    obj = obj[prop];
+  });
+  return obj;
+};
+priceCalc.output.price = function () {//replace input.calc() or spinoff some functionality to it?!!
+  var obj = priceCalc.output.obj();
+  var h = $('.price-calc .height').val() || 1;
+  var w = $('.price-calc .width').val() || 1;
+  var qty = $('.price-calc .qty').val() || 0;
+  var totalArea = h * w * qty; 
+  var base = priceCalc.logic.getBasePrice(obj, totalArea);//adjust for base price here!!
+  //if upgrade, add to base price
+  var price = base * totalArea;
+  //if extra addons, add to price
+  $('.price-calc .price').html('Total: $' + price.toFixed(2));
+};
+priceCalc.output.qty = function (obj) {
+  var label = $('<label>');
+  var desc = $('<span>').html('Quantity: ');
+  var input = $('<input>').attr('type', 'number').addClass('qty').attr('min', 1).val(1).attr('oninput', 'priceCalc.input.calc()');
+  return label.append(desc).append(input);
 };
